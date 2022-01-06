cc8a0e2cfec062f0c291374996925583d057307d
Nat! <nat@mulle-kybernetik.com>
2022-01-06 22:08:26 +0100

    add EXTERN_GLOBAL

--- src/include.h
+++ src/include.h
@@ -29,6 +29,11 @@
 
 #include "_mulle-fprintf-include.h"
 
+#ifndef MULLE_FPRINTF_EXTERN_GLOBAL
+# define MULLE_FPRINTF_EXTERN_GLOBAL MULLE_C_EXTERN_GLOBAL
+#endif
+
+
 /* You can add some more include statements here */
 
 #endif
