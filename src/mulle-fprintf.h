f64f60cdf49fb38000d41d2b52bd1145baef3afb
Nat! <nat@mulle-kybernetik.com>
2023-04-06 01:26:30 +0200

    modernized project

--- src/mulle-fprintf.h
+++ src/mulle-fprintf.h
@@ -14,7 +14,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_FPRINTF_VERSION  ((0 << 20) | (1 << 8) | 1)
+#define MULLE_FPRINTF_VERSION  ((0 << 20) | (1 << 8) | 2)
 
 
 static inline unsigned int   mulle_fprintf_get_version_major( void)
